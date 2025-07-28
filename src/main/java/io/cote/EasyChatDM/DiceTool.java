cf05343c840454412df78110ed02fb82c9bbe2ed
Coté <github@coteindustries.com>
2025-07-28 15:54:35 +0200

    Added format presevation tags.

--- src/main/java/io/cote/EasyChatDM/DiceTool.java
+++ src/main/java/io/cote/EasyChatDM/DiceTool.java
@@ -4,10 +4,8 @@ import com.bernardomg.tabletop.dice.history.RollHistory;
 import com.bernardomg.tabletop.dice.interpreter.DiceRoller;
 import com.bernardomg.tabletop.dice.parser.DefaultDiceParser;
 import com.bernardomg.tabletop.dice.parser.DiceParser;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Service;
@@ -17,16 +15,17 @@ public class DiceTool {
 
     private static final Logger logger = LoggerFactory.getLogger(DiceTool.class);
 
+    //@formatter:off
     @Tool(name = "EasyChatDM_rollDice",
           description = """
                   Rolls dice according to the syntax defined in the D&D 5e rules.
                   Returns total of all dice rolled.
                   """)
     public String roll(@ToolParam(description =
-                                    "Notation for dice to roll such a d6, 2d4+4, 3d6, d20-3, 1d20+7 etc.")
-                                  String diceExpression,
-                       @ToolParam(description = MCPUtils.INTENT_DESCRIPTION, required = true) String intent) {
-
+                       "Notation for dice to roll such a d6, 2d4+4, 3d6, d20-3, 1d20+7 etc.")
+                        String diceExpression,
+                       @ToolParam(description = MCPUtils.INTENT_DESCRIPTION) String intent) {
+    //@formatter:on
         DiceParser diceParser = new DefaultDiceParser();
         // DiceParser does not like spaces ¯\_(ツ)_/¯
         String nospaces = diceExpression.replaceAll("\\s", "");
